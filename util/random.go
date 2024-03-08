e64885313306cb8c6d22eb07e0d4d72a6a0c8a80
Steven Yi <siyuan.yi@vanderbilt.edu>
2024-03-07 23:38:02 -0600

    Add user feature

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -46,4 +47,9 @@ func RandomCurrency() string {
 	currencies := []string{USD, EUR, CNY}
 	n := len(currencies)
 	return currencies[r.Intn(n)]
+}
+
+// RandomEmail generates a random email address
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
